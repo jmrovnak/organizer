9152a775b87a770a253c9eade2d0bbbab1bc711b
Justin Rovnak <jmrovnak@gmail.com>
2021-01-18 21:37:42 -0600

    added funtion to display current date on page load

--- organizer.js
+++ organizer.js
@@ -1,5 +1,12 @@
-var current = document.querySelector("#current");
-
 window.addEventListener("load", function () {
     console.log("test")
 })
+
+window.addEventListener("load", function () {
+    var today = new Date();
+    var date = (today.getMonth()+1) + "/" + today.getDate();
+    console.log(date);
+    var current = document.querySelector("#current");
+    current.innerHTML = date;
+})
+

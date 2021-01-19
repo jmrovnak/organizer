85374d0c7b2ab5b7ec72db634652e0b8ba5713ed
Justin Rovnak <jmrovnak@gmail.com>
2021-01-18 22:11:01 -0600

    added current time to load load event

--- organizer.js
+++ organizer.js
@@ -6,7 +6,16 @@ window.addEventListener("load", function () {
     var today = new Date();
     var date = (today.getMonth()+1) + "/" + today.getDate();
     console.log(date);
-    var current = document.querySelector("#current");
-    current.innerHTML = date;
+    
+    var currentdate = document.querySelector("#currentDate");
+    currentdate.innerHTML = date;
+
+    var time = today.getHours() + ":" + today.getMinutes();
+    var currenttime = document.querySelector("#currentTime");
+    currenttime.innerHTML = time;
+
+
+
+
 })
 

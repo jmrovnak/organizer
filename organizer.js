4bd46abafcc7db3398a1bbe7e8d3bb021a508829
Justin Rovnak <jmrovnak@gmail.com>
2021-01-20 22:24:31 -0600

    sketched the rest of the time blocks

--- organizer.js
+++ organizer.js
@@ -1,21 +1,20 @@
-var today = moment().format("MMMM Do YYYY");
-console.log(today);
+var today = moment().format("MMMM Do YYYY"); 
+console.log(today); //testing moment functionality
 
 var date = document.querySelector("#current");
 window.addEventListener("load", function () {
     current.innerHTML = today;
-});
+  }); // displays date on page load
 
 
 var time = moment().format("HH:MM");
-console.log(time);
+console.log(time); //testing moment functionality
 
-var blockOne = document.querySelector("#blockOne");
-
-var store = document.querySelector(".saveBtn");
+// storing input in local storage
+var blockOne = document.querySelector("#blockOne");var store = document.querySelector(".saveBtn");
 store.addEventListener("click", function () {
     localStorage.setItem("9:00", (blockOne.value));
-})
+}) 
 
-
+blockOne.append(localStorage.getItem("9:00"));
 

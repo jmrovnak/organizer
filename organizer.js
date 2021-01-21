19e528fd215e042c541b81721fe6d1f9442ddc9b
Justin Rovnak <jmrovnak@gmail.com>
2021-01-20 21:22:31 -0600

    added current time, 4-5. set input to store in local storage, 9-11

--- organizer.js
+++ organizer.js
@@ -1,3 +1,15 @@
 var today = moment().format("MMMM Do YYYY");
 console.log(today);
 
+var time = moment().format("HH:MM");
+console.log(time);
+
+var blockOne = document.querySelector("#blockOne");
+
+var store = document.querySelector(".saveBtn");
+store.addEventListener("click", function () {
+    localStorage.setItem("9:00", (blockOne.value));
+})
+
+
+
